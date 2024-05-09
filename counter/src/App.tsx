2600ee2c734a0384cc762a84295cdc0d8f08b637
Johnnypham2 <phammjohnny@gmail.com>
2024-05-08 18:47:31 -0700

    first commit

---
+++ counter/src/App.tsx
@@ -0,0 +1,8 @@
+import Message from "./Message";
+import Counter from "./components/Counter";
+
+function App()
+{
+  return <div><Counter/></div>
+}
+export default App;
---
+++ counter/src/components/Counter.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+
+const Counter = () => {
+  const [counter, setCounter] = useState(0);
+
+  const increaseNumber = () => {
+    setCounter(counter + 1);
+  };
+
+  const decreaseNumber = () => {
+    setCounter((prevCounter) => (prevCounter > 0 ? prevCounter - 1 : 0));
+  };
+
+  return (
+    <>
+      <div className="d-grid gap-2 col-6 mx-auto">
+        <h1>Counter: {counter}</h1>
+        <button
+          className="btn btn-primary"
+          type="button"
+          onClick={increaseNumber}
+        >
+          Increase me bruh
+        </button>
+        <button
+          className="btn btn-danger"
+          type="button"
+          onClick={decreaseNumber}
+        >
+          Decrease me breh
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default Counter;
